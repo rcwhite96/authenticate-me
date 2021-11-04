b65274d4a59060ae78e3ac318b0b64885b30af63
rachel <rachelcwhite1996@gmail.com>
2021-11-04 13:10:05 -0400

    login validation

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -3,12 +3,28 @@ const asyncHandler = require('express-async-handler');
 
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
+
+//this checks the keys of of credential with username, email and password
+//this must come before the log in
+const validateLogin = [
+    check('credential')
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .withMessage('Please provide a valid email or username.'),
+    check('password')
+      .exists({ checkFalsy: true })
+      .withMessage('Please provide a password.'),
+    handleValidationErrors,
+  ];
 
 // Log in
 router.post(
     '/',
+    validateLogin,
     asyncHandler(async (req, res, next) => {
       const { credential, password } = req.body;
 
@@ -29,7 +45,6 @@ router.post(
       });
     }),
   );
-
 //Log out
 router.delete(
   '/',
@@ -52,5 +67,5 @@ router.get(
       } else return res.json({});
     }
   );
-  
+
 module.exports = router;
