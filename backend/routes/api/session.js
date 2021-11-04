6934409c7ca5e0d1b6175ab1b0a8a431b5f9d9ee
rachel <rachelcwhite1996@gmail.com>
2021-11-04 13:02:18 -0400

    restore session user

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -29,7 +29,7 @@ router.post(
       });
     }),
   );
-  
+
 //Log out
 router.delete(
   '/',
@@ -38,4 +38,19 @@ router.delete(
     return res.json({ message: 'success' });
   }
 );
+
+// Restore session user
+router.get(
+    '/',
+    restoreUser,
+    (req, res) => {
+      const { user } = req;
+      if (user) {
+        return res.json({
+          user: user.toSafeObject()
+        });
+      } else return res.json({});
+    }
+  );
+  
 module.exports = router;
