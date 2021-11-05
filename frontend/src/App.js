7b62561e8b878ee57b543d514cd21fb2d8b6fee1
rachel <rachelcwhite1996@gmail.com>
2021-11-05 15:42:37 -0400

    part 3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import LoginFormPage from "./components/LoginFormPage";
 import SignupFormPage from "./components/SignupFormPage";
+import SplashPage from "./components/SplashPage"
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
 
@@ -18,6 +19,9 @@ function App() {
       <Navigation isLoaded={isLoaded} />
       {isLoaded && (
         <Switch>
+          <Route path="/" exact>
+            <SplashPage/>
+          </Route>
           <Route path="/login">
             <LoginFormPage />
           </Route>
---
+++ frontend/src/components/SplashPage/index.js
@@ -0,0 +1,14 @@
+import react from 'react'
+import logo from '../../yarniconwhite.png'
+import './splashpage.css'
+
+function SplashPage() {
+    return(
+        <div className="logo-div">
+            <img src={logo} alt="Logo" className="splash-logo"/>
+            <h2 className="h2">Keep your inspirations here.</h2>
+        </div>
+    )
+}
+
+export default SplashPage
