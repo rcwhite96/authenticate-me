80fd2498830f6be68bb587d7cc9ed5101bb2625a
rachel <rachelcwhite1996@gmail.com>
2022-01-24 13:09:43 -0500

    metadata

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,11 +4,15 @@ const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const notebookRouter = require('./notebooks.js')
 const noteRouter = require('./notes')
+const noteSearch = require('./search')
+
 
 router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 router.use('/notebooks', notebookRouter)
 router.use('/notes', noteRouter)
+router.use('/search', noteSearch)
+
 
 
 module.exports = router;
